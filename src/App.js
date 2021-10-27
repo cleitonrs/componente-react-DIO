2e077b7dfcc22d6a714b42b54ca29dac2bbc3a92
cleitonrs <rscleitonrs@gmail.com>
2021-10-27 15:01:43 -0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import Item from "./components/item"
+
 const App = () => {
-  return(
-    <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3
+        </Item>
+      </ul>
+    </>
   )
 }
 
